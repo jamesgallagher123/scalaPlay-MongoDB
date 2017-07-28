89f1858f164e076d0cedf3cdf04013c93ff3917e
jamesgallagher123 <james.gallagher@qa.com>
2017-07-28 15:50:53 +0100

    end of day commit

--- app/controllers/MongoApplicationController.scala
+++ app/controllers/MongoApplicationController.scala
@@ -3,7 +3,7 @@ package controllers
 import javax.inject.Inject
 
 import scala.concurrent.Future
-import play.api.mvc.{Action, AnyContent, Controller}
+import play.api.mvc._
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.libs.json._
 import reactivemongo.api.Cursor
@@ -12,21 +12,46 @@ import models.JsonFormats._
 import play.modules.reactivemongo.{MongoController, ReactiveMongoApi, ReactiveMongoComponents}
 import reactivemongo.play.json._
 import collection._
+import play.api.i18n.{I18nSupport, MessagesApi}
 
-class MongoApplicationController @Inject()(val reactiveMongoApi: ReactiveMongoApi) extends Controller
-  with MongoController with ReactiveMongoComponents {
+class MongoApplicationController @Inject()(val reactiveMongoApi: ReactiveMongoApi, val messagesApi: MessagesApi) extends Controller
+  with MongoController with ReactiveMongoComponents with I18nSupport {
 
   // TODO - keep in mind you need to have mongod.exe running before attempting to play around
 
   def collection: Future[JSONCollection] = database.map(_.collection[JSONCollection]("persons"))
 
-  def create: Action[AnyContent] = Action.async {
-    //case class User(age: Int, firstName: String, lastName: String, feeds: Seq[Feed])
-    //case class Feed(name: String, url: String)
-    val user = User("FirstName", "Lastname", 28)
-    val futureResult = collection.flatMap(_.insert(user))
-    futureResult.map(_ => Ok("Added user " + user.firstName + " " + user.lastName))
+  def loadCreateUser = Action { implicit request =>
+    Ok(views.html.createUser(User.createUserForm))
   }
+
+  def createUser: Action[AnyContent] = Action.async {
+    //case class User(firstName: String, lastName: String, age: Int)
+    val user = User()
+    val futureResult = collection.flatMap(_.insert(user))
+
+    val formValidationResult = User.createUserForm.bindFromRequest
+    formValidationResult.fold({ formWithErrors =>
+      BadRequest(views.html.createUser(formWithErrors))
+    }, { user =>
+      //add here
+
+    })
+
+    futureResult.map(_ => Ok("Added user " + user.firstName + " " + user.lastName))
+
+  }
+
+
+
+
+
+
+
+
+
+
+
 
   def findByName: Action[AnyContent] = Action.async {
     val cursor: Future[Cursor[User]] = collection.map {
--- app/models/User.scala
+++ app/models/User.scala
@@ -1,10 +1,25 @@
 package models
 
+import play.api.data._
+import play.api.data.Forms._
+
 case class User(
  firstName: String,
  lastName: String,
  age: Int
 )
+
+object User {
+
+  val createUserForm = Form(
+    mapping(
+      "firstName" -> nonEmptyText,
+      "lastName" -> nonEmptyText,
+      "age" -> number(min = 0, max = 120)
+    )(User.apply)(User.unapply)
+  )
+
+}
 
 object JsonFormats {
   import play.api.libs.json.Json
